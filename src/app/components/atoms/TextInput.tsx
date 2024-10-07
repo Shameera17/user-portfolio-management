c87827a0521c7c4eda34bc2ffc8a764b6997c325
Shameera17 <shameera.carrim17@gmail.com>
2024-10-07 16:25:11 +0530

    fix type error

--- src/app/components/atoms/TextInput.tsx
+++ src/app/components/atoms/TextInput.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
-interface InputProps {
+interface TextInputProps {
   type?: string;
   placeholder?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value?: string;
 }
-export function Email(props: InputProps) {
+export function Email(props: TextInputProps) {
   return <Input type="email" placeholder={props.placeholder || "Email"} />;
 }
-export function Password(props: InputProps) {
+export function Password(props: TextInputProps) {
   return (
     <Input
       value={props.value}
