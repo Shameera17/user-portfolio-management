2966af74e58485719bf1108f752fb99da7932afa
Shameera17 <shameera.carrim17@gmail.com>
2024-10-09 17:18:58 +0530

    modified types

--- src/app/client-provider.tsx
+++ src/app/client-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 export default function Provider({
@@ -7,7 +8,7 @@ export default function Provider({
   session,
 }: {
   children: React.ReactNode;
-  session: any;
+  session: Session | null;
 }): React.ReactNode {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
